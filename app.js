3e6d32852a768cc312c8ee970fbca1a40f43772b
amnaalrashid <amna.alrashid@gmail.com>
2024-10-04 22:39:02 +0300

    Done EH and NFH middlewares

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const postsRoutes = require('./api/posts/posts.routes');
-const connectDb = require('./database');
+const postsRoutes = require("./api/posts/posts.routes");
+const connectDb = require("./database");
+const cors = require("cors");
+const morgan = require("morgan");
+const notFoundHandler = require("./middlewares/notFoundHandler");
+const dotenv = require("dotenv");
 
+dotenv.config();
 connectDb();
 app.use(express.json());
-app.use('/posts', postsRoutes);
+app.use("/posts", postsRoutes);
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/posts", postsRoutes);
+app.use(cors());
+app.use(morgan("dev"));
+app.use(notFoundHandler);
 
-app.listen(8000, () => {
-  console.log('The application is running on localhost:8000');
+app.listen(process.env.PORT, () => {
+  console.log(`The application is running on localhost:${process.env.PORT}`);
 });
--- database.js
+++ database.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-const connectDB = async () => {
-  const conn = await mongoose.connect("YOUR_CONNECTION_STRING");
-  console.log(`mongo connected: ${conn.connection.host}`);
+dotenv.config();
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    throw error;
+  }
 };
 
-module.exports = connectDB;
+module.exports = connectDb;
